13e4ff0931de0716b2c290652449ece44d645cb7
cassandrasantander <cassandrasantander03@gmail.com>
2026-07-27 00:21:19 -0700

    Update loginMechanic.js

--- loginMechanic.js
+++ loginMechanic.js
@@ -2,7 +2,7 @@ function checkInput(){
 			var userN = document.getElementById('username').value;
 			var passW = document.getElementById('password').value;
 			if (userN==="archivist019" && passW==="manatee"){
-				document.location="basenotes.html";
+				document.location='basenotes.html';
 			}else
 				alert("Wrong username and/or password.");
 }
