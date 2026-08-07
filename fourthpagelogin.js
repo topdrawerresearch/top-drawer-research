6dd2a6d99372363e27025cf8d1ad046d46406ca1
Air-Conditioning <theriddlers714@gmail.com>
2026-08-07 00:25:01 -0700

    Fourth + Fifth page
    Created 4th page to take binary number puzzle password to page 5

---
+++ fourthpagelogin.js
@@ -0,0 +1,7 @@
+function checkInput(){
+			var passW = document.getElementById('password').value;
+			if (passW==="PX07TD8BPZXD3"){
+				document.location='fifthpage.html';
+			}else
+				alert("Wrong password.");
+}
